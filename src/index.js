d02b9e2d473c78cac97f9eca459464ed5fe2c3d3
nikachello <nikain35@gmail.com>
2025-10-16 11:24:55 +0400

    changed builds

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import authRoutes from "./routes/auth.js";
-const app = express();
-app.use(express.json());
-// Mount all /api/auth/* requests to authRoutes
-app.use("/api/auth", authRoutes);
-app.listen(3000, () => console.log("Server running on port 3000"));
-//# sourceMappingURL=index.js.map
